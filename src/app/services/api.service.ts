f12b89e4ccf3285cdba6090a5a0c3beeabf1fe62
karabocodes <145660355+karabocodes@users.noreply.github.com>
2024-09-18 05:58:14 +0200

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://nagarroapi.onrender.com';
 
   constructor(private http: HttpClient) { }
 
